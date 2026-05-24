55d7e169b0aef6752c1706ab1ace8e8fee443333
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2026-05-24 10:27:08 -0400

    Remove webpage routes

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import { generate } from './generate';
-import index from './index.html'; 
 
 const server = Bun.serve({
   port: 3000,
   routes: {
-    "/": index,
     "/api/pack": async () => {
         return Response.json(await generate())
     }
